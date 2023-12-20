32dd93dd5621ec171cb36e43ab4a57498c41d614
Jalajakshi D S <114593975+Jalajakshids20@users.noreply.github.com>
2023-12-20 15:51:33 +0530

    Add files via upload

---
+++ Message.js
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import pen from "../images/pen.png"
+import { TextField } from '@mui/material';
+import { addDoc, collection, doc } from 'firebase/firestore';
+import { auth, database } from '../firebase/setup';
+
+const style = {
+  position: 'absolute',
+  top: '61%',
+  left: '71%',
+  transform: 'translate(-50%, -50%)',
+  width: "40vw",
+  height: "35vw",
+  minHeight: "505px",
+  bgcolor: 'background.paper',
+  padding: "1vw",
+};
+
+export default function Message() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setMailId(""); // Reset mailId state
+    setMessage(""); // Reset message state
+  };
+
+  const [mailId, setMailId] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  const send = async () => {
+    const messageRefSend = collection(database, "Users", `${auth.currentUser?.email}`, "Send");
+    try {
+      await addDoc(messageRefSend, {
+        email: message
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const inbox = async () => {
+    const userDoc = doc(database, "Users", `${mailId}`);
+    const messageRefInbox = collection(userDoc, "Inbox");
+    try {
+      await addDoc(messageRefInbox, {
+        email: message,
+        sender: auth.currentUser?.displayName
+      });
+      await send(); // Wait for the send function to complete
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <div onClick={handleOpen} style={{
+        cursor: 'pointer', height: "4.5vw", marginLeft: "1vw",
+        width: "12vw", display: "flex", alignItems: "center",
+        borderRadius: "20px", backgroundColor: "#BEE0FF"
+      }}>
+        <img src={pen} style={{ width: "1.2vw", marginLeft: "2vw" }} alt='text' />
+        <h4 style={{ marginLeft: "1.6vw", fontWeight: "400", fontSize: '1.3vw' }}>Compose</h4>
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography style={{
+            backgroundColor: "#EDF9FF", position: 'absolute',
+            top: "0", left: "0", width: "41vw", padding: "0.5vw", fontSize: "1vw"
+          }}>
+            New Message
+          </Typography>
+          <TextField onChange={(e) => setMailId(e.target.value)} variant='standard' label="To" sx={{ width: "39vw", marginTop: "1vw" }} />
+          <br />
+          <TextField variant='standard' label="Subject" sx={{ width: "39vw" }} />
+          <br />
+          <TextField onChange={(e) => setMessage(e.target.value)} multiline rows={12} sx={{ width: "39vw", "& fieldset": { border: "none" } }} />
+          <br />
+          <Button onClick={inbox} variant='contained' sx={{
+            borderRadius: "6vw",
+            fontSize: "1vw", width: "4vw", height: "3vw"
+          }}>Send</Button>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
